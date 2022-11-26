681b14ee787f25d990f3e481f40d58cbc7fa85fa
Trisha Karani <tkarani1@jhu.edu>
2022-11-26 18:16:20 -0500

    check for sid

--- HW4_ShowRawScores.php
+++ HW4_ShowRawScores.php
@@ -20,31 +20,36 @@
        // call the stored procedure we already defined on dbase
 	   if ($result = $conn->query("CALL ShowRawScores('".$item."');")) {
 
-	      echo "<table border=\"2px solid black\">";
+			if ($result !== 'SID ERR') {
 
-          // output a row of table headers
-	      echo "<tr>";
-	      // collect an array holding all attribute names in $result
-	      $flist = $result->fetch_fields();
-          // output the name of each attribute in flist
-	      foreach($flist as $fname){
-	         echo "<td>".$fname->name."</td>";
-	      }
-	      echo "</tr>";
+				echo "<table border=\"2px solid black\">";
 
-          // output a row of table for each row in result, using flist names
-          // to obtain the appropriate attribute value for each column
-	      foreach($result as $row){
+				// output a row of table headers
+				echo "<tr>";
+				// collect an array holding all attribute names in $result
+				$flist = $result->fetch_fields();
+				// output the name of each attribute in flist
+				foreach($flist as $fname){
+					echo "<td>".$fname->name."</td>";
+				}
+				echo "</tr>";
 
-              // reset the attribute names array
-    	      $flist = $result->fetch_fields(); 
-	          echo "<tr>";
-	          foreach($flist as $fname){
-                      echo "<td>".$row[$fname->name]."</td>";
-              }
-  	          echo "</tr>";
-	      }
-	      echo "</table>";
+				// output a row of table for each row in result, using flist names
+				// to obtain the appropriate attribute value for each column
+				foreach($result as $row){
+
+					// reset the attribute names array
+					$flist = $result->fetch_fields(); 
+					echo "<tr>";
+					foreach($flist as $fname){
+							echo "<td>".$row[$fname->name]."</td>";
+					}
+					echo "</tr>";
+				}
+				echo "</table>";
+			} else {
+				echo $result
+			}
 
           } else {
              echo "Call to ShowRawScores failed<br>";
--- HW4_ShowRawScores.sql
+++ HW4_ShowRawScores.sql
@@ -10,47 +10,51 @@ DELIMITER //
 CREATE PROCEDURE ShowRawScores(IN sid VARCHAR(10))
 
 BEGIN
-   SET @sql = NULL;
+    IF (SELECT COUNT(buyerNum) FROM Bid WHERE itemID = item > 0) THEN
+        SET @sql = NULL;
 
-   -- accumulate into the variable named @sql a list of assignment names
-   -- and expressions to that will get the associated scores, for use 
-   -- as part of a later query of table HW4_RawScore
-   SELECT
-     GROUP_CONCAT(DISTINCT
-       CONCAT(
-         'max(case when aname = ''',
-         aname,
-         ''' then score end) as ''',aname,''''
-       )
-       ORDER BY atype DESC, aname ASC
-     ) INTO @sql
-   FROM HW4_Assignment;
+        -- accumulate into the variable named @sql a list of assignment names
+        -- and expressions to that will get the associated scores, for use 
+        -- as part of a later query of table HW4_RawScore
+        SELECT
+            GROUP_CONCAT(DISTINCT
+            CONCAT(
+                'max(case when aname = ''',
+                aname,
+                ''' then score end) as ''',aname,''''
+            )
+            ORDER BY atype DESC, aname ASC
+            ) INTO @sql
+        FROM HW4_Assignment;
 
-   -- concatenate the assignment name list and associated expressions
-   -- into a larger query string so we can execute it, but leave ?
-   -- in place so we can plug in the specific sid value in a careful way
-    SET @sql = CONCAT('SELECT ',
-                     @sql,
-                     ' FROM HW4_RawScore WHERE sid = ',
-		     '?');
+        -- concatenate the assignment name list and associated expressions
+        -- into a larger query string so we can execute it, but leave ?
+        -- in place so we can plug in the specific sid value in a careful way
+            SET @sql = CONCAT('SELECT ',
+                            @sql,
+                            ' FROM HW4_RawScore WHERE sid = ',
+                    '?');
 
-   SET @sql = CONCAT('WITH SInfo AS (SELECT sid, LName, FName, Sec
-                  FROM HW4_Student WHERE sid = ?),',
-                  'SScores AS (', @sql, ')',
-                  'SELECT * FROM SInfo JOIN SScores');
-   -- alert the server we have a statement shell to set up
-   PREPARE stmt FROM @sql;
+        SET @sql = CONCAT('WITH SInfo AS (SELECT sid, LName, FName, Sec
+                        FROM HW4_Student WHERE sid = ?),',
+                        'SScores AS (', @sql, ')',
+                        'SELECT * FROM SInfo JOIN SScores');
+        -- alert the server we have a statement shell to set up
+        PREPARE stmt FROM @sql;
 
-   -- now execute the statement shell with a value plugged in for the ?
-   EXECUTE stmt USING sid, sid;
+        -- now execute the statement shell with a value plugged in for the ?
+        EXECUTE stmt USING sid, sid;
 
-   -- tear down the prepared shell since no longer needed (we won't requery it)
-   DEALLOCATE PREPARE stmt;
+        -- tear down the prepared shell since no longer needed (we won't requery it)
+        DEALLOCATE PREPARE stmt;
 
-    -- this is another way to structure the query
-    -- SELECT LName, FName, Sec, HW4_RawScore.sid, max(case when aname = 'quiz1' then score end) as 'quiz1'
-    --        FROM HW4_RawScore JOIN HW4_Student ON HW4_RawScore.sid = HW4_Student.sid
-    --        WHERE HW4_RawScore.sid = @sid
+            -- this is another way to structure the query
+            -- SELECT LName, FName, Sec, HW4_RawScore.sid, max(case when aname = 'quiz1' then score end) as 'quiz1'
+            --        FROM HW4_RawScore JOIN HW4_Student ON HW4_RawScore.sid = HW4_Student.sid
+            --        WHERE HW4_RawScore.sid = @sid
+    ELSE 
+        SELECT 'SID ERR';
+    END IF;
 
 END; //
 
